952aef48b6a93660624d2946917417e425179bb4
carloshn90 <carlohn90@gmail.com>
2020-06-16 16:31:31 +0100

    Updates

---
+++ precache-manifest.da210fd3ae41ed8f898f79ab01b728ae.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "683992217caf330640a2c64d289348b1",
+    "url": "/path-finder/index.html"
+  },
+  {
+    "revision": "1d5f95653cdb9a9cd953",
+    "url": "/path-finder/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "6ac0fbd5b2580a4556a9",
+    "url": "/path-finder/static/css/main.affc669f.chunk.css"
+  },
+  {
+    "revision": "1d5f95653cdb9a9cd953",
+    "url": "/path-finder/static/js/2.cb0de5bd.chunk.js"
+  },
+  {
+    "revision": "90565ba0b43e1b95d2876295dfd7014d",
+    "url": "/path-finder/static/js/2.cb0de5bd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6ac0fbd5b2580a4556a9",
+    "url": "/path-finder/static/js/main.b3d887eb.chunk.js"
+  },
+  {
+    "revision": "76193e0e06112c5d40ab",
+    "url": "/path-finder/static/js/runtime-main.9031676a.js"
+  }
+]);
